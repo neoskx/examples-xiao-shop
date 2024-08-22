5afb9a43c6b3a52545dd89134e3b3100a9b62212
Neo Xu <neo@Neos-MacBook-Pro.local>
2024-08-21 23:38:57 -0700

    Add a basic structure and add product service

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # xiao-shop
+
 This is an mock application that I used to learn large-scale distributed system
+
+## Overview
+
+Goal: Create an e-commerce platform with the following microservices:
+
+1.	Product Service: Manages product catalog (CRUD operations).
+2.	Order Service: Handles order creation, management, and history.
+3.	User Service: Manages user authentication and profiles.
+4.	Payment Service: Processes payments for orders.
+
+## Technologies:
+
+* Programming Language: Node.js (Express.js for API development).
+* Database: PostgreSQL (for relational data) and Redis (for caching).
+* Message Queue: Apache Kafka (for asynchronous messaging).
+* Containerization: Docker.
+* Orchestration: Kubernetes on Azure (AKS).
+* Helm: For Kubernetes package management.
+* CI/CD: GitHub Actions for continuous integration and deployment.
---
+++ services/product/routes/products.js
@@ -0,0 +1,60 @@
+const express = require('express');
+// const debug = require('debug')('product:api');
+const { Pool } = require('pg');
+
+const router = express.Router();
+console.log(process.env.DATABASE_URL);
+let _pool;
+
+async function getPGPool() {
+  if (_pool) {
+    return _pool;
+  }
+  _pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+  });
+
+  // TODO: Handle error
+  try {
+    await new Promise((resolve, reject) => {
+      _pool.connect((err, client, done) => {
+        if (err){
+          reject(err);
+          throw err;
+        }
+        client.query('SELECT NOW()', (err, res) => {
+          done(); // release the client back to the pool
+          if (err) {
+            console.log(err.stack);
+            reject(err);
+          } else {
+            console.log(res.rows[0]);
+            resolve(true);
+          }
+        });
+      });
+    });
+  } catch (err) {
+    throw err;
+  }
+
+  return _pool;
+}
+
+router.get('/', async (req, res) => {
+  const pool = await getPGPool();
+  const { rows } = await pool.query('SELECT * FROM products');
+  res.send(rows);
+});
+
+router.post('/', async (req, res) => {
+  const pool = await getPGPool();
+  const { name, price } = req.body;
+  const { rows } = await pool.query(
+    'INSERT INTO products (name, price) VALUES ($1, $2) RETURNING *',
+    [name, price],
+  );
+  res.send(rows[0]);
+});
+
+module.exports = router;
